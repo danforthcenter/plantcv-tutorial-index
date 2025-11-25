c0543c7fe2f75bdab2769249b68baeb9c128b248
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-11-25 10:56:21 -0600

    bash syntax

--- test_tutorials.sh
+++ test_tutorials.sh
@@ -4,12 +4,12 @@
 # will run on the first of every month per:
 #
 
-triggered_time = `date +'%Y-%m-%d-%H-%M-%S'`
+triggered_time=`date +'%Y-%m-%d-%H-%M-%S'`
 
 for dir in /data/datascience/users/jsumner/plantcv-tutorial-index/repos/*/; do
 	   cd $dir
 	   git pull origin main
-	   echo triggered_time > "auto_run"
+	   echo $triggered_time > "auto_run"
 	   git add .
 	   git commit -m "Auto commit on $triggered_time"
 	   git push origin main
